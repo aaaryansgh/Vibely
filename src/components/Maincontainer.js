f079716f24fccbc5ab29c8dfc3e73ef723ccb1fb
Aaryan Singh <singharyan1817@outlook.com>
2025-04-30 21:12:15 +0530

    Updated the mainMovie component

--- src/components/Maincontainer.js
+++ src/components/Maincontainer.js
@@ -6,7 +6,7 @@ import Videoback from './videoback'
 const Maincontainer = () => {
     const movies=useSelector((store)=>store.movie?.nowPlayingMovies)
     if(!movies) return; //if movie is not present then return. it won't throw an error
-    const mainMovie=movies[0];
+    const mainMovie=movies[2];
     const {original_title,overview}=mainMovie
   return (
     <div className='pt-[10%] md:pt-0 bg-black'>
